4c31f735cd1f2fe81ffb83633f3f9b7693f2a5e2
chinedugabriel <chinedugabriel29@gmail.com>
2023-05-22 18:24:10 +0100

    added a toggle list manu and it repective icons

---
+++ js/frutta.js
@@ -0,0 +1,8 @@
+// variables 
+let navToggleIcon = document.getElementById("toggle-menu-icon");
+
+let secondMenuList = document.getElementById("second-menu-list");
+
+navToggleIcon.addEventListener('click',()=>{
+    navToggleIcon.src="img/icons/close-menu.png";
+});
