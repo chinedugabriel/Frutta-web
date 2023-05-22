79ce6c58cdbef9933d1dbf0b04385822e6edcd0a
chinedugabriel <chinedugabriel29@gmail.com>
2023-05-22 18:53:58 +0100

    added a toggle function for the menu list on mobile

--- js/frutta.js
+++ js/frutta.js
@@ -1,8 +1,19 @@
 // variables 
+
+// Navigation Icon
 let navToggleIcon = document.getElementById("toggle-menu-icon");
 
+// Menu list
 let secondMenuList = document.getElementById("second-menu-list");
 
+// This event listener hides and displays the mobile menu List
 navToggleIcon.addEventListener('click',()=>{
-    navToggleIcon.src="img/icons/close-menu.png";
+    
+    if (secondMenuList.style.display === "none"){
+        secondMenuList.style.display = "block";
+        navToggleIcon.src="img/icons/close-menu.png";
+    }else{
+        secondMenuList.style.display = "none";
+        navToggleIcon.src="img/icons/menu-toggle.png";
+    }
 });
