c9269b13ffa4cff12ca2dbbce9e8f0f054a79603
chinedugabriel <chinedugabriel29@gmail.com>
2023-05-29 20:46:21 +0100

    added template on product, About Us, and Contactus page

--- js/frutta.js
+++ js/frutta.js
@@ -14,10 +14,10 @@ navToggleIcon.addEventListener('click',()=>{
     
     if (secondMenuList.style.display === "none"){
         secondMenuList.style.display = "block";
-        navToggleIcon.src="img/icons/close-menu.png";
+        navToggleIcon.src="../img/icons/close-menu.png";
     }else{
         secondMenuList.style.display = "none";
-        navToggleIcon.src="img/icons/menu-toggle.png";
+        navToggleIcon.src="../img/icons/menu-toggle.png";
     }
 });
 
